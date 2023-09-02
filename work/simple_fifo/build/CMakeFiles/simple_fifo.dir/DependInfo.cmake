d02bae2500ef4d28a0debf2d58792441edabea0a
Allen Jiang <alljiang@hotmail.com>
2023-09-02 00:57:28 -0500

    q2b

---
+++ work/simple_fifo/build/CMakeFiles/simple_fifo.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/Github/ECE382N-SoC-Design/work/simple_fifo/simple_fifo.cpp" "CMakeFiles/simple_fifo.dir/simple_fifo.cpp.o" "gcc" "CMakeFiles/simple_fifo.dir/simple_fifo.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
